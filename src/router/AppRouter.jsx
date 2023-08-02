d5af89133f49c08104eb5fb36cb89bcbc1e9e390
Murat <murttkapln@gmail.com>
2023-08-03 00:41:10 +0300

    first commit

---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import Register from "../pages/Register";
+import Login from "../pages/Login";
+import Main from "../pages/Main";
+import MovieDetail from "../pages/MovieDetail";
+import { Route, Routes } from "react-router-dom";
+
+
+const AppRouter = () => {
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Main/>} />
+        <Route path="/login" element={<Login/>} />
+        <Route path="/register" element={<Register/>} />
+        <Route path="/details/:id" element={<MovieDetail/>} />
+      </Routes>
+    </>
+  );
+};
+
+export default AppRouter;
